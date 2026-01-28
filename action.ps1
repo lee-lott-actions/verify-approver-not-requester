197c0fbe2ff1fa8b22827aef1aaca8e21f8ee7de
Lee Lott <168018332+Lee-Lott@users.noreply.github.com>
2026-01-28 07:52:47 -0600

    Refactor verification logic with error handling

--- action.ps1
+++ action.ps1
@@ -13,14 +13,19 @@ function Verify-ApproverNotRequestor {
     return
   }
 
-  if ($Requester -ne $Approver) {    
-    Add-Content -Path $env:GITHUB_OUTPUT -Value "result=success"
-    Add-Content -Path $env:GITHUB_OUTPUT -Value "is-approver-not-requester=true"
-    Write-Host "Verification passed: Approver ($Approver) is different from requester ($Requester)."    
-  } else {
-    Write-Host "Approver ($Approver) cannot be the same as the requester ($Requester)."
-    Add-Content -Path $env:GITHUB_OUTPUT -Value "result=success"
-    Add-Content -Path $env:GITHUB_OUTPUT -Value "is-approver-not-requester=false"    
+  try {
+    if ($Requester -ne $Approver) {    
+      Add-Content -Path $env:GITHUB_OUTPUT -Value "result=success"
+      Add-Content -Path $env:GITHUB_OUTPUT -Value "is-approver-not-requester=true"
+      Write-Host "Verification passed: Approver ($Approver) is different from requester ($Requester)."    
+    } else {
+      Write-Host "Approver ($Approver) cannot be the same as the requester ($Requester)."
+      Add-Content -Path $env:GITHUB_OUTPUT -Value "result=success"
+      Add-Content -Path $env:GITHUB_OUTPUT -Value "is-approver-not-requester=false"    
+    }
+  } catch {
+      Write-Host "Failed to verify requestor is not approver."
+      Add-Content -Path $env:GITHUB_OUTPUT -Value "result=failure"
+      Add-Content -Path $env:GITHUB_OUTPUT -Value "is-approver-not-requester=false"    
   }
 }
-
